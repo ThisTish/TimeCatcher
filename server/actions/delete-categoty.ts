470dbcfda6795833ff17cc7476ceed6c1f095abc
Tish <tishthegoat@gmail.com>
2024-12-07 14:32:23 -0500

    safeAction delete refactor

--- server/actions/delete-categoty.ts
+++ server/actions/delete-categoty.ts
@@ -1,17 +1,38 @@
 "use server"
 
 import { db } from "@/prisma/db"
+import { createSafeActionClient } from "next-safe-action"
+import * as z from 'zod'
 
-const deleteCategory = async (id: string ) =>{
-	const deletingCategory = await db.category.delete({
-		where: {
-			id
+const action = createSafeActionClient()
+
+const deleteCategory = action
+	.schema(z.object({id: z.string()}))
+	.action(async ({parsedInput: {id}}) =>{
+		try {
+			const data = await db.category.delete({
+				where:{
+					id
+				}
+			})
+
+			return {success: `Category ${data.name} deleted successfully`}
+
+		} catch (error) {
+			return { error: 'Error occurred while deleting category'}
 		}
 	})
 
-	if(!deletingCategory) return {error: 'Category not found'}
+// const deleteCategory = async (id: string ) =>{
+// 	const deletingCategory = await db.category.delete({
+// 		where: {
+// 			id
+// 		}
+// 	})
 
-	return deletingCategory
-}
+// 	if(!deletingCategory) return {error: 'Category not found'}
+
+// 	return deletingCategory
+// }
 
 export default deleteCategory
