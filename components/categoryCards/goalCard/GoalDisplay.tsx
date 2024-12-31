2139d49209d888649c60b4afa38961841ecca401
Tish <tishthegoat@gmail.com>
2024-12-31 02:30:32 -0700

    time format string

--- components/categoryCards/goalCard/GoalDisplay.tsx
+++ components/categoryCards/goalCard/GoalDisplay.tsx
@@ -1,15 +1,57 @@
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
+import { timeFormat } from "@/lib/time-format"
+import { TimeFrame } from "@prisma/client"
 import { Edit } from "lucide-react"
 
-const GoalDisplay = () => {
+type GoalDisplayProps = {
+	timeFrame: TimeFrame
+	timePassed: number
+	targetTime: number
+}
+
+
+const GoalDisplay = ({timeFrame, timePassed, targetTime}: GoalDisplayProps) => {
+	// console.log(timeFrame, timePassed, targetTime)
+
+	const timeFormatString = ({ time, h, m, includeSeconds, s }: { time: number, h: string, m: string, includeSeconds: boolean, s?: string }) => {
+		console.log(timeFormat(time / 1000))
+		if (time === 0) {
+			if (includeSeconds) return `0${h} 0${m} 0${s}`
+			return `0${h} 0${m}`
+		}
+		const { hours, minutes, seconds } = timeFormat(time / 1000)
+
+		if (hours === 0 && minutes === 0) {
+			if (includeSeconds) return `${minutes}${m} ${seconds}${s}`
+			return `0${h} 0${m}`
+		}
+
+		if(hours === 0){
+			if(includeSeconds) return `${minutes}${m} ${seconds}${s}`
+			return `${minutes}${m} `
+		} 
+
+		if (minutes === 0){
+			if(includeSeconds) return `${hours}${h} ${minutes}${m} ${seconds}${s}`
+			return `${hours}${h}`
+		}
+
+		if (includeSeconds) return `${hours}${h} ${minutes}${m} ${seconds}${s}`
+		
+		return `${hours}${h} ${minutes}${m}`
+	}
+
+
+	const timeToGo = targetTime - timePassed
+
 	return (
 		<figure className="w-full -ml-1 relative">
 			<header>
-				<h4 className="text-sm -ml-3 font-semibold">timeFrame</h4>
+				<h4 className="text-sm -ml-3 font-semibold">{timeFrame}</h4>
 			</header>
 			<div className="flex items-center justify-center text-xs gap-1">
-				<span>1hr</span>
+				<span className="text-nowrap">{timeFormatString({ time: timePassed, h: 'h', m: 'm', includeSeconds: false })}</span>
 				
 				<Progress 
 				className="border border-black"
@@ -17,11 +59,11 @@ const GoalDisplay = () => {
 				
 				/>
 				
-				<span >20hr</span>
+				<span className="text-nowrap">{timeFormatString({ time: targetTime, h: 'h', m: 'm', includeSeconds: false, })}</span>
 				<Edit className="absolute size-4 -right-5"/>
 			</div>
 			<footer>
-				<p className="text-center text-xs tracking-tighter leading-none"><strong>19</strong> hours to go</p>
+				<p className="text-center text-xs tracking-tighter leading-none">{timeFormatString({ time: timeToGo, h: ` hours`, m: ` minutes`, includeSeconds: false })} to go!</p>
 				{/* <p className="text-center text-sm bg-white">to go`</p> */}
 			</footer>
 		</figure>
--- components/categoryCards/goalCard/GoalsCard.tsx
+++ components/categoryCards/goalCard/GoalsCard.tsx
@@ -22,25 +22,22 @@ type GoalCardProps = {
 
 const GoalCards = ({ goals, color, timePassed }: GoalCardProps) => {
 
-
+	const activeGoals = goals?.filter((goal) => goal.active)
 
 	return (
 		<Card className={`${backgrounds[color]} rounded-md size-64 flex flex-col justify-around relative`}>
 			<CardHeader className="pt-2 pb-1 mb-1 text-lg font-bold text-center border border-b-2">
-			<h3>GOALS</h3>
+				<h3>GOALS</h3>
 			</CardHeader>
 			{/* running timer display */}
 			<CardContent className="grid gap-1 tabular-nums">
-				{/* {goals?.map((goal) => ( */}
-					<GoalDisplay
-					/>
-					<GoalDisplay
-					/>
-					<GoalDisplay
-					/>
-					<GoalDisplay
-					/>
-{/* ))} */}
+				{activeGoals?.map((goal) => (
+					goal.timeFrame === TimeFrame.YEAR ? (
+						<GoalDisplay key={goal.id} timeFrame='YEAR' timePassed={360000} targetTime={2400000} />
+					) : null
+				))
+			}
+				
 
 
 			</CardContent>
