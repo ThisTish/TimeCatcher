8285d615fb7741254ebc6b11907a97da9f349a98
Tish <tishthegoat@gmail.com>
2025-01-27 00:16:45 -0700

    md responsive flippy clock

--- components/guestPage/FlipCard.tsx
+++ components/guestPage/FlipCard.tsx
@@ -22,33 +22,33 @@ const FlipCard = ({ currentDigit, prevDigit, index }: { currentDigit: string, pr
 		<div className={cn(
 			` relative flex flex-col h-12 sm:h-24  md:h-36 bg-slate-200 rounded-lg shadow-lg text-3xl sm:text-6xl  md:text-8xl  font-semibold`,
 			currentDigit === 'AM' || currentDigit === 'PM'
-				? 'w-16 sm:32 md:w-48' : 'w-10 sm:w-16 md:w-24',
+				? 'w-16 sm:w-32 md:w-48' : 'w-10 sm:w-16 md:w-24',
 			index == 1 || index == 2 || index == 4 || index == 6
 				? 'ml-0' : 'ml-1 md:ml-2',
 
 		)}>
 			{/* Top Card */}
 			<div className=" relative w-full h-1/2 bg-slate-300 rounded-t-md overflow-hidden shadow-md ">
-				<p className="absolute top-1 left-2  md:top-4 md:left-2 tabular-nums">
+				<p className="absolute top-1 sm:top-[1rem] md:top-4 left-2 tabular-nums">
 					{currentDigit}
 				</p>
 			</div>
 
 			{/* Bottom Card */}
 			<div className=" relative w-full h-1/2 bg-slate-200 rounded-b-md overflow-hidden shadow-md">
-				<p className="absolute -top-5 left-2 md:-top-14 md:left-2  tabular-nums">
+				<p className="absolute -top-5 sm:-top-8 md:-top-14 left-2  tabular-nums">
 					{prevDigit}
 				</p>
 			</div>
 
 			{/* Animation Layer */}
-			{/* {isFlipped ? null : (
+			{isFlipped ? null : (
 
 				<div
-					className='absolute w-full h-full top-0 left-0 overflow-hidden'>
+					className='absolute w-full h-full top-0 left-0 '>
 					<div
-						className={`animate-flip-down bg-slate-300 w-full h-1/2 transform origin-bottom`}>
-						<p className=" absolute top-1 left-2 md:top-4 md:left-2 tabular-nums">
+						className={`animate-flip-down bg-slate-300 overflow-hidden w-full h-1/2 transform origin-bottom`}>
+						<p className=" absolute top-1 sm:top-[1rem] md:top-4 left-2 tabular-nums">
 							{prevDigit}
 						</p>
 					</div>
@@ -59,12 +59,12 @@ const FlipCard = ({ currentDigit, prevDigit, index }: { currentDigit: string, pr
 				<div className={` absolute w-full h-1/2 bottom-0 left-0 overflow-hidden animate-flip-up transform origin-top`}>
 					<div
 						className={` bg-slate-200 w-full h-1/2 `}>
-						<p className="absolute -top-5 left-2 md:-top-14 md:left-2 tabular-nums">
+						<p className="absolute -top-5 sm:-top-8 md:-top-14 left-2 tabular-nums">
 							{currentDigit}
 						</p>
 					</div>
 				</div>
-			)} */}
+			)}
 		</div>
 
 	)
